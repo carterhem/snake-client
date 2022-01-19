56076876715a7ecb8f45ce6920a9543478d3a4c3
Carter Hemington <carter.hemington@gmail.com>
2022-01-19 22:51:29 +0000

    adding input file

--- play.js
+++ play.js
@@ -1,10 +1,12 @@
 const net = require("net");
 const {connect} = require('./client')
+const {setupInput} = require('./input')
 // establishes a connection with the game server
-
-
-
+// setup interface to handle user input from stdin
 
 
 console.log("Connecting ...");
 connect();
+
+
+setupInput()
